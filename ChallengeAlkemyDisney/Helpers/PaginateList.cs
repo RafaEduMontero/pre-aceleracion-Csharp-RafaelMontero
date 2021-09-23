3040e96f3a8a28aceac1fe353287df3c5b4cb458
Rafael Montero <rafaedumontero@gmail.com>
2021-09-23 12:19:05 -0300

    helper class PaginateList was added

---
+++ ChallengeAlkemyDisney/Helpers/PaginateList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeAlkemyDisney.Helpers
+{
+    public class PaginateList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PagesSize { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasPrevious => (CurrentPage > 1);
+        public bool HasNext => (CurrentPage < TotalPages);
+
+        public PaginateList(List<T> items, int count , int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PagesSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)PagesSize);
+            AddRange(items);
+        }
+
+        public static PaginateList<T> Create(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return new PaginateList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
